e7b6109c605a184ef6354c07a9bc155ac4650571
satoshi <kawakami.cf@gmail.com>
2016-02-10 14:24:06 +0900

    heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 end
 class Micropost < ActiveRecord::Base
 	belongs_to :User
-	validates :content, length: { maximum: 140 } #投稿を140文字以内に制限
+	validates :content, length: { maximum: 140 } #投稿を140文字以内に制限。
 end
 
